4c81622475a9690fb1bdd648b25f1bf9f5770a52
sourabbr <sourabbr@gmail.com>
2020-08-10 00:04:39 +0530

    Updated ghost, library and templates

--- build.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-folder=$1
-
-mkdir ${folder}/bin
-g++ -I -Wall ${folder}/src/*.cpp -o ${folder}/bin/output.exe
---
+++ ghost.sh
@@ -0,0 +1,106 @@
+#! /bin/bash
+
+tool_name="ghost"
+
+CheckCmdStatus () {
+
+    if [ $1 -ne 0 ]
+    then
+        echo "=========FAILED!!"
+        exit -1
+    fi
+}
+
+Clean () {
+
+    echo "=========CLEANING..."
+    rm -rf $1/bin ; CheckCmdStatus $?
+
+    echo "=========DONE!!"
+    return 0
+}
+
+Compile () {
+
+    dir=$1
+
+    echo "=========Creating bin directory"
+    mkdir -p ${dir}/bin ; CheckCmdStatus $?
+
+    echo "=========COMPILING..."
+    g++ -o ${dir}/bin/output.exe -Wall -I library ${dir}/src/*.cpp ; CheckCmdStatus $?
+
+    echo "=========SUCCESS!!"
+    return 0
+}
+
+Create () {
+
+    echo "=========CREATING PROJECT.."
+    mkdir -p $1 ; CheckCmdStatus $?
+    mkdir -p $1/src ; CheckCmdStatus $?
+    touch $1/src/main.cpp ; CheckCmdStatus $?
+    cat templates/main_cpp.cpp > $1/src/main.cpp ; CheckCmdStatus $?
+    
+    echo "=========DONE!!"
+    return 0
+}
+
+Delete () {
+
+    echo "=========DELETING PROJECT.."
+    rm -rf $1 ; CheckCmdStatus $?
+    
+    echo "=========DONE!!"
+    return 0
+}
+
+Help () {
+
+    echo "=========COMPILE"
+    echo "About: Compiles a project"
+    echo "Usage: $tool_name compile project_name"
+    echo "=========CLEAN"
+    echo "About: Cleans the project"
+    echo "Usage: $tool_name clean project_name"
+    echo "=========CREATE"
+    echo "About: Creates a project"
+    echo "Usage: $tool_name create project_name"
+    echo "=========DELETE"
+    echo "About: Deletes a project"
+    echo "Usage: $tool_name delete project_name"
+    echo "=========HELP"
+    echo "About: What $tool_name can do"
+    echo "Usage: $tool_name help"
+    return 0
+}
+
+if [ $1 == "compile" ]
+then
+    Compile $2
+    exit $?
+fi
+
+if [ $1 == "clean" ]
+then
+    Clean $2
+    exit $?
+fi
+
+if [ $1 == "create" ]
+then
+    Create $2
+    exit $?
+fi
+
+if [ $1 == "delete" ]
+then
+    Delete $2
+    exit $?
+fi
+
+if [ $1 == "help" ]
+then
+    Help
+    exit $?
+fi
